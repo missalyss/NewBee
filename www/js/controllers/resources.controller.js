6760fe7675a1a777fe817e3d17d0e70e91329e7b
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-08 09:23:02 -0700

    adjusted colors and fonts

--- www/js/controllers/resources.controller.js
+++ www/js/controllers/resources.controller.js
@@ -6,10 +6,10 @@ function($scope, $stateParams, $http) {
   const apiUrl = `https://bee-md.herokuapp.com`
 
   // $scope.getAllSymptoms = function() {
-    $http.get(`${apiUrl}/symptoms`).then(result => {
-      $scope.data = result.data
-      console.log($scope.data);
-    })
+    // $http.get(`${apiUrl}/symptoms`).then(result => {
+    //   $scope.data = result.data
+    //   console.log($scope.data);
+    // })
   // }
 
     $http.get(`${apiUrl}/causes`).then(result => {
