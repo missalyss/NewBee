9f69dd838af555c07a9e0e586e18cce9c3abef9d
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-10 23:42:51 -0700

    barebones structure for monitor base tab

--- www/js/controllers/monitor.controller.js
+++ www/js/controllers/monitor.controller.js
@@ -1,16 +1,12 @@
 angular.module('app.monitor', [])
 
-.controller('monitorCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('monitorCtrl', function($scope, monitorService) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+
+  // $scope.$on('$ionicView.enter', function() {
+    // monitorService.all().then(result => {
+      // console.log(result.data);
+    // })
+  // });
+
 })
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -8,3 +8,12 @@ angular.module('app.services', [])
     }
   }
 })
+
+.factory('monitorService', function ($http) {
+  const apiUrl = 'https://internet-of-stings.herokuapp.com/humiture'
+  return {
+    all: function() {
+      return $http.get(apiUrl)
+    }
+  }
+})
